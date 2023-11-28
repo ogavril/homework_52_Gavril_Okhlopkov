3792924b5576e9b1c734dcd3df160818012e32d9
Gavril <gokhlopkov2759@gmail.com>
2023-11-28 15:03:28 +0600

    Добавил в webapp/urls.py path('delete/', delete_list),

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views import index_view, list_add
+from webapp.views import index_view, list_add, delete_list
 
 urlpatterns = [
     path('', index_view),
-    path('add/', list_add)
+    path('add/', list_add),
+    path('delete/', delete_list),
 ]
