6a1fbdf40317438db1239557ece77a689ecf4171
Gavril <gokhlopkov2759@gmail.com>
2023-11-28 12:47:31 +0600

    Создал index_view в view.py и index.html

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from webapp.models import List
+
 
 # Create your views here.
+
+
+def index_view(request):
+    lists = List.objects.all
+    return render(request, 'index.html', {'lists': lists})
