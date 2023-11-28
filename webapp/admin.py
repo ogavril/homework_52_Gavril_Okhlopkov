bbc2ccd326b2bc322bc55544088a04cf01112894
Gavril <gokhlopkov2759@gmail.com>
2023-11-28 11:17:32 +0600

    Добавил модель List в файл admin.py, поменял тип поля DateTimeField на DateField

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from webapp.models import List
 
 # Register your models here
+
+admin.site.register(List)
--- webapp/models.py
+++ webapp/models.py
@@ -8,4 +8,4 @@ class List(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Описание")
     status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
     status = models.CharField(max_length=25, choices=status_choices, default='new', verbose_name="Статус")
-    due_date = models.DateTimeField(null=True, blank=True, verbose_name="Дата выполнения")
+    due_date = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
