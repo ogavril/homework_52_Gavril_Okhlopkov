e0bb1c018d9ca0c048672b040cf4b110b723a03c
Gavril <gokhlopkov2759@gmail.com>
2023-11-28 13:45:35 +0600

    Добавил в views.py list_add для добавления новых задач

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from webapp.models import List
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -8,3 +9,15 @@ from webapp.models import List
 def index_view(request):
     lists = List.objects.all
     return render(request, 'index.html', {'lists': lists})
+
+
+def list_add(request):
+    if request.method == "GET":
+        return render(request, 'add.html')
+    elif request.method == "POST":
+        List.objects.create(
+            description=request.POST.get('description'),
+            status=request.POST.get('status'),
+            due_date=request.POST.get('due_date'),
+        )
+        return HttpResponseRedirect('/')
