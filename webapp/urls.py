aeeeed0388f59e660feee3452f37d65895166e41
Gavril <gokhlopkov2759@gmail.com>
2023-11-28 13:46:27 +0600

    Добавил в webapp/urls.py url '/add'

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import index_view, list_add
 
 urlpatterns = [
     path('', index_view),
+    path('add/', list_add)
 ]
