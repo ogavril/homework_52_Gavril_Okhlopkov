d9ee15938590f481276d9780d9690e391384d92a
Gavril <gokhlopkov2759@gmail.com>
2023-12-29 14:48:09 +0600

    добавил перенаправления

--- homework/settings.py
+++ homework/settings.py
@@ -118,3 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'accounts:login'
+
+LOGIN_REDIRECT_URL = 'projects'
+LOGOUT_REDIRECT_URL = 'accounts:login'
